8c588b06d192abdd6e254501084146d8dae1dac4
sammilam <124639069+sammilam@users.noreply.github.com>
2023-03-23 15:52:52 -0400

    transfer

--- 03.15/sketch.js
+++ 03.15/sketch.js
@@ -1,10 +1,6 @@
 function setup(){
     createCanvas(1000, 1000);
     background ("white");
-    console.log ("hello");
-}
-
-function draw(){
     noStroke();
     fill("#ffb703");
     ellipse (485, height/2, 500);
@@ -13,6 +9,10 @@ function draw(){
     fill("white");
     quad(width/2, height/2, width/2, 500, 900, 250, 800, height/2);
     fill("#ffb703");
-    rect(480, height/2, 235, 80);
+    rect(500, height/2, 215, 100);
+}
+
+function draw(){
+  
 
 }
