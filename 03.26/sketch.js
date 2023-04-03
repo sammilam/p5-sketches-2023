30dca9ec1505383919b959a27e3c1fd787747b74
sammilam <124639069+sammilam@users.noreply.github.com>
2023-04-03 17:41:41 -0400

    updates

--- 03.26/sketch.js
+++ 03.26/sketch.js
@@ -1,1 +1,30 @@
+let img;
 
+function preload(){
+
+    img = loadImage('g.jpg');
+}
+
+
+function setup(){
+    createCanvas(1000, 1000);
+    background ("black");
+
+
+}
+
+function draw(){
+  
+  image(img, 0, 0, width, height);
+  let space = floor(10 + mouseX/20);
+  for (let i = 0; i < width; i += space) {
+    for (let j = 0; j < height; j+= space) {
+      let imgX = map(i, 0, width, 0, img.width);
+      let imgY = map(j, 0, height, 0, img.height);
+
+      let c = img.get(imgX, imgY);
+      fill(c);
+      circle(i, j, space);
+    }
+  }
+}
---
+++ 03.28/sketch.js
@@ -0,0 +1,28 @@
+let x, y;
+let size = 50;
+let size2 = 25;
+
+function setup() {
+  createCanvas(1000, 1000);
+  x = width / 2;
+  y = height / 2;
+  noStroke();
+}
+
+function draw() {
+  background("#E8B4BC");
+  fill("#3a3238");
+  ellipse(x, y, size);
+  fill("#E8B4BC");
+  ellipse(x, y, size2);
+  fill("#E8B4BC");
+  triangle(x, y + 10, x + size, y - size, x + size, y + 10);
+  fill("#3a3238");
+  rect(x, y, 80+size, 20);
+
+  size += 5;
+  size2 += 0.25;
+  if (size >= min(width, height)) {
+    size = 50;
+  }
+}
