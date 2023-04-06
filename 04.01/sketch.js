e78ea93c4d97aff0c3c0d349b326665453c743f7
sammilam <124639069+sammilam@users.noreply.github.com>
2023-04-06 04:55:17 -0400

    new

--- 04.01/sketch.js
+++ 04.01/sketch.js
@@ -1,33 +1,39 @@
-let numLayers = 2;
+let numLayers = 5;
 let angle = 0;
-let speed = 0.1;
+let speed = 0.03;
 let gText;
+let x = 0;
+let y = 0;
+
+let numRows = 7;
+let numCols = 7;
+let gridSpacing = 150;
 
 function preload() {
-  gText = loadFont('Monoton.ttf');
+  gText = loadFont('uni.ttf');
 }
 
 function setup() {
   createCanvas(1000, 1000, WEBGL);
   textAlign(CENTER, CENTER);
-  textSize(500);
+  textSize(150);
   textFont(gText);
 }
 
 function draw() {
   background(0);
 
-  for (let i = 0; i < numLayers; i++) {
-    let layerPos = map(i, 0, numLayers - 1, 0, height);
-    for (let j = 0; j < numLayers; j++) {
-      let colPos = map(j, 0, numLayers - 1, 0, width);
+  for (let i = 0; i < numRows; i++) {
+    for (let j = 0; j < numCols; j++) {
+      let posX = map(j, 0, numCols - 1, -gridSpacing * (numCols - 1) / 2, gridSpacing * (numCols - 1) / 2);
+      let posY = map(i, 0, numRows - 1, -gridSpacing * (numRows - 1) / 2, gridSpacing * (numRows - 1) / 2);
+
       push();
-      rotateX(angle + i * 20);
-      let r = map(i, 0, numLayers, 255, 0);
-      let g = map(j, 0, numLayers, 0, 255);
-      let b = map(i + j, 0, numLayers * 2, 0, 255);
-      fill(r, g, b);
-      text("G", 0, 0);
+      translate(posX, posY);
+
+      // Draw shapeG
+      shapeG();
+
       pop();
     }
   }
@@ -35,5 +41,23 @@ function draw() {
   angle += speed;
   if (angle > 360) {
     angle = 0;
+  }
+}
+
+function shapeG() {
+  for (let i = 0; i < numLayers; i++) {
+    let layerPos = map(i, 0, numLayers - 1, 0, height);
+    for (let j = 0; j < numLayers; j++) {
+      let colPos = map(j, 0, numLayers - 1, 0, width);
+
+      push();
+      rotateX(angle + i * 20);
+      let r = map(i, 0, numLayers, 255, 0);
+      let g = map(j, 0, numLayers, 0, 255);
+      let b = map(i + j, 0, numLayers, 0, 255);
+      fill(r, g, b);
+      text("G", 0, 0);
+      pop();
+    }
   }
 }
--- 04.02/sketch.js
+++ 04.02/sketch.js
@@ -1,51 +1,39 @@
+let gText;
+let stretchAmount = 1;
+let stretchSpeed = 0.05;
+let fr = 30;
+let sound;
+
+function preload() {
+  gText = loadFont('uni.ttf');
+  sound = loadSound('lofi.mp3');
+}
+
 function setup() {
-    createCanvas(600, 600);
-  }
+  createCanvas(1000, 1000);
+  textAlign(CENTER, CENTER);
+  textSize(200);
+  textFont(gText);
+  frameRate(fr);
   
-  function draw() {
-    background(30);
-    noStroke();
-    
-    // Define the vertex points for the "G" shape
-    let gShape = [
-      createVector(0, 0),
-      createVector(100, 0),
-      createVector(100, 50),
-      createVector(50, 50),
-      createVector(50, 100),
-      createVector(0, 100)
-    ];
-  
-    // Set the number of "G" shapes to draw
-    let numG = 10;
-    
-    // Loop through the number of "G" shapes to draw
-    for (let i = 0; i < numG; i++) {
-      // Set the position and size of the "G" shape
-      let xPos = map(i, 0, numG-1, 50, width-50);
-      let yPos = height/2;
-      let size = map(sin(frameCount/10 + i), -1, 1, 30, 80);
-  
-      // Set the rotation of the "G" shape
-      let rotation = map(sin(frameCount/20 + i), -1, 1, -45, 45);
-  
-      // Translate and rotate the canvas to the position and rotation of the "G" shape
-      push();
-      translate(xPos, yPos);
-      rotate(rotation);
-  
-      // Scale the "G" shape to the desired size
-      scale(size/100);
-  
-      // Draw the "G" shape using the defined vertex points
-      beginShape();
-      for (let j = 0; j < gShape.length; j++) {
-        vertex(gShape[j].x, gShape[j].y);
-      }
-      endShape(CLOSE);
-  
-      // Restore the canvas to its original position and rotation
-      pop();
-    }
-  }
-  
+}
+
+function draw() {
+  background(255);
+  noStroke();
+
+  stretchAmount = map(sin(frameCount * stretchSpeed), -1, 1, 1, 2);
+
+  push();
+  translate(width / 2, height / 2);
+  scale(stretchAmount, random(10));
+  text("G", 0, 0);
+  pop();
+
+      sound.play();
+}
+
+
+
+
+// Music by <a href="https://pixabay.com/users/fassounds-3433550/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=140858">FASSounds</a> from <a href="https://pixabay.com//?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=music&amp;utm_content=140858">Pixabay</a>
