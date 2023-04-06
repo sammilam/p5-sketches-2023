26928b13274828e94e11bf00eda2191900577501
sammilam <124639069+sammilam@users.noreply.github.com>
2023-04-06 16:25:54 -0400

    new

--- 04.03/sketch.js
+++ 04.03/sketch.js
@@ -1,34 +1,37 @@
-let numLayers = 10;
+let numLayers = 5;
 let angle = 0;
-let speed = 0.1;
+let speed = 0.025;
 let gText;
+let x = -500;
+let y = -500;
+
 
 function preload() {
-  gText = loadFont('uni.ttf');
+  gText = loadFont('poppin.ttf');
 }
 
 function setup() {
-  createCanvas(1000, 1000, WEBGL);
+  c = createCanvas(1000, 1000, WEBGL);
   textAlign(CENTER, CENTER);
   textSize(500);
   textFont(gText);
+  background(0);
+
 }
 
 function draw() {
-  background(0);
-
-  for (let i = 0; i < numLayers; i++) {
+    for (let i = 0; i < numLayers; i++) {
     let layerPos = map(i, 0, numLayers - 1, 0, height);
     for (let j = 0; j < numLayers; j++) {
       let colPos = map(j, 0, numLayers - 1, 0, width);
       push();
       translate(colPos, layerPos);
-      rotateX(angle + i * 20);
+      rotateX(angle + i );
       let r = map(i, 0, numLayers - 1, 255, 0);
       let g = map(j, 0, numLayers - 1, 0, 255);
       let b = map(i + j, 0, numLayers * 2 - 2, 255, 0);
       fill(r, g, b);
-      text("G", 0, 0);
+      text("G", x, y);
       pop();
     }
   }
@@ -39,3 +42,7 @@ function draw() {
   }
 }
 
+
+function mousePressed() {
+  saveCanvas(c, "fresh-sketch", "png");
+}
